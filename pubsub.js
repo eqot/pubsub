cefd6a5e15b41a398ad49e91e2544b40358cbc99
Ikuo Terado <eqobar@gmail.com>
2015-09-20 23:36:32 +0900

    Minor changes

--- pubsub.js
+++ pubsub.js
@@ -8,14 +8,14 @@ var pubsub = function () {
   var create = function (server) {
     io = require('socket.io')(server);
 
-    io.on('connection', function(socket){
+    io.on('connection', function (socket) {
       console.log('a user connected');
 
-      socket.on('disconnect', function(){
+      socket.on('disconnect', function () {
         console.log('user disconnected');
       });
 
-      socket.on('chat message', function(msg){
+      socket.on('chat message', function (msg) {
         console.log('message: ' + msg);
 
         io.emit('chat message', msg);
