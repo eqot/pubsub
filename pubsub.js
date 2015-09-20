f4f57973f67524b7deb3ff0615a4d9c2e4383160
Ikuo Terado <eqobar@gmail.com>
2015-09-20 23:15:57 +0900

    Support disconnection

--- pubsub.js
+++ pubsub.js
@@ -10,6 +10,10 @@ var pubsub = function () {
 
     io.on('connection', function(socket){
       console.log('a user connected');
+
+      socket.on('disconnect', function(){
+        console.log('user disconnected');
+      });
     });
   };
 
