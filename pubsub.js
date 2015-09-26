a3d3bc5ee60ab724dc02d781d3b8ea6aea2eb674
Ikuo Terado <eqobar@gmail.com>
2015-09-26 15:10:05 +0900

    Use polling transport in Socket.IO for proxy environment

--- pubsub.js
+++ pubsub.js
@@ -6,7 +6,7 @@ var pubsub = function () {
   var io = null;
 
   var create = function (server) {
-    io = require('socket.io')(server);
+    io = require('socket.io')(server, {transports: ['polling']});
 
     io.on('connection', function (socket) {
       console.log('a user connected');
